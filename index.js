38f4c6b0c465a0218bc65a74e604cf4f93501bde
SerGG52151 <a01571538@tec.mx>
2025-03-13 21:07:59 -0600

    Frontend Login Funcional

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import express from 'express';
 
+const app = express();
+const port = 4000;
+
+app.use("/", express.static("public"));
+app.listen(4000, console.log("http://localhost:" + port));
--- public/pages/home.js
+++ public/pages/home.js
@@ -2,25 +2,35 @@ const btnLogin = document.getElementById("btnLogin");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
+const url = "http://localhost:5000";
+
 correctUsername = "admin";
 correctPassword = "admin";
 
-function login(username, password){
-    if(username == "admin" && password == "admin"){
-        sessionStorage.setItem("name", "Sergio G.");
+const login = async () => {
+    const user = { username: username.value, password: password.value };
+    const respuesta = await fetch(url + "/login", {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(user),
+    });
+    const data = await respuesta.json();
+    if(data.isLogin){
+        sessionStorage.setItem("id", data.user.id);
+        sessionStorage.setItem("name", data.user.name);
         alert("Login Successful");
-        window.location.href = "../index.html";
+        window.location.href = "../index.html?id=" + data.user.id;
     } else {
         alert("Login Failed");
     }
-}
+};
 
 btnLogin.addEventListener("click", function() {
-    login(username.value, password.value);
+    login();
 });
 
 password.addEventListener("keydown", (e) => {
     if(e.key == "Enter"){
-        login(username.value, password.value);
+        login();
     }
 });
